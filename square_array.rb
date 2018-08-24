95484fce12744e91ae3b9a649996ae3f3f41a6ac
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-08-24 05:12:06 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,10 @@ numbers = [1,2,3]
 new_numbers = [9,10,16,25]
 
 def square_array(array)
-  arre = []
+  arr = []
   array.each{|i| arr << (i**2)}
   print arre
 end
 
 square_array(numbers)
-print arre
+print arr
